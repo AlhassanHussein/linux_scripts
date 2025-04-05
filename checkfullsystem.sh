ce715a3cd190ca71095155c190e0f7a995a8dc34
AlhassanHussein <python.hassan@gmail.com>
2025-04-05 19:58:51 +0200

    update check full system script

--- checkfullsystem.sh
+++ checkfullsystem.sh
@@ -1,25 +1,76 @@
 #!/bin/bash
 
-echo "=========== CPU Info =======";
-lscpu;
-
-echo -e "\n======= GPU Info =========";
-lspci | grep -i --color 'vga\|3d\|2d';
+#  ====== FUNCTIONS =====
 
 
-echo -e "======== RAM Info ========";
-free -h;
+cpu_info(){
+	echo "======= CPU INFO =======";
+	lscpu;
+	echo "/n";
+}
+
+ram_info(){
+	
+	echo "======= CPU INFO =======";
+	free -h;
+	echo "/n"
+}
+
+disk_info(){
+	echo "======= CPU INFO ======="
+	df -h
+	echo "\n"
+}
+
+os_info(){
+	echo "======= CPU INFO =======";
+	cat /etc/os-release;
+	echo "\n";
+}
+
+all_info(){
+	cpu_info;
+	ram_info;
+	disk_info;
+	os_info;
+
+}
+quit(){
+break
+}
 
 
-echo -e "\n ========Disk Info ======";
-lsblk
 
 
-echo -e "\n=========OS Info ======";
-cat /etc/os-release;
+show_menu(){
+	echo "==============";
+	echo " SELECT YOUR OPTION";
+	echo "==============";
+	echo "1) CPU INFO";
+	echo "2) RAM INFO";
+	echo "3) DISK INFO";
+	echo "4) OS INFO";
+	echo "0) ALL INFORMATION";
+	echo "q|Q) EXIT";
 
-echo -e "\n======== Kernel Version =====";
-uname -r
+}
+invaild_option(){
+	echo "INVAILD OPTION";
+}
 
-echo -e "\n ======= UPtime ========";
-uptime
+while true; do 
+	show_menu
+
+	read -p "pleas your choice" choice; 
+
+	case $choice in
+		 1) cpu_info;;
+		 2) ram_info;;
+		 3) disk_info;;
+		 4) os_info;;
+		 0) all_info;;
+	 	 q|Q) break;;
+		 *)invaild_option;;
+	esac
+done
+
