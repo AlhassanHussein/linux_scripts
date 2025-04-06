2dae4b545e68dfc7e7cc1f5a1ea585ccfa9794fc
AlhassanHussein <python.hassan@gmail.com>
2025-04-06 14:56:59 +0200

    add demo company

---
+++ demo_company/scripts/create_random_passwd.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "This script create a RANDOM PASSWORD to the input file and pass it to output file";
+echo "CURRANT PATH:$(pwd)";
+
+echo "Please enter the pass of the users file";
+
+read user_input_path;
+echo "$user_input_path";
+users_output_path="users_output_path.txt";
+echo "1";
+
+> "$users_output_path";
+echo "2"
+
+while read -r user_name; do
+	echo "$user_name";
+	echo "3"
+	sudo useradd  -m  "$user_name";
+	echo "$(id) $user_name";
+	echo "4";
+	password=$(openssl rand -base64 12);
+	echo "5";
+	echo "$user_name:$password" | sudo chpasswd;
+	echo "6";
+	echo "$user_name:$password" >> "$users_output_path";
+done < "$user_input_path"
+echo "7";
